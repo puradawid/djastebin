60bfd837196505a8238d3e4994af5c6509872668
Paweł Kondraciuk <kondraciukpawel@gmail.com>
2014-04-03 21:15:37 +0200

    Templates added

--- src/apps/home/views.py
+++ src/apps/home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, World")
+    return render(request, 'home/index.html')
