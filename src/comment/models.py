efc9f2944c91c6b9f2651b4474eeb0f0d6326685
Adrian Rutkowski <adrian.rutkowsky@gmail.com>
2014-04-03 10:29:12 -0700

    Added basic model (Paste and Comment)

--- src/comment/models.py
+++ src/comment/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from paste.models import Paste
 
-# Create your models here.
+class Comment(models.Model):
+    created = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    author = models.ForeignKey(User)
+    paste = models.ForeignKey(Paste)
+    parent = models.ForeignKey('self', null=True, blank=True, default=None)
+    
+    def __unicode__(self):
+        return self.date
