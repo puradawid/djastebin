73661af0691d92e625836dfa3f9422d56decd600
Paweł Kondraciuk <kondraciukpawel@gmail.com>
2014-04-06 22:14:21 +0200

    Add user pastes and pagination

--- src/apps/users/views.py
+++ src/apps/users/views.py
@@ -1,18 +1,30 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.views.generic.base import View
 from apps.users.forms import UserRegistrationForm
 from django.contrib.auth import authenticate, login, logout
 from django.http.response import HttpResponseRedirect
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
+from apps.pastes.models import Paste
+from django.views.generic.list import ListView
 
 # Create your views here.
 
-class UserDetailsView(View):
-    def get(self, request, user_name):
-        return render(request, 'users/user.html', {'user_name' : user_name})
-    def post(self, request, user_name):
-        return render(request, 'users/user.html', {'user_name' : user_name})
+class UserDetailsView(ListView):
+    model = Paste
+    template_name = 'users/user_pastes.html'
+    context_object_name = 'paste_list'
+    paginate_by = 1
     
+    def get_queryset(self):
+        return Paste.objects.filter(author=User.objects.filter(username=self.kwargs['user_name']))
+    
+    def get_context_data(self, **kwargs):
+        context = super(UserDetailsView, self).get_context_data(**kwargs)
+        context['user_profile'] = User.objects.get(username=self.kwargs['user_name'])
+        context['total_pastes'] = self.get_queryset().count()
+        return context
+
 class SettingsView(View):
     def get(self, request):
         return render(request, 'users/user.html', {'user_name' : 'settings_get'})
@@ -20,9 +32,9 @@ class SettingsView(View):
         return render(request, 'users/user.html', {'user_name' : 'settings_post'})
 
 class ProfileView(View):
-    def get(self, request, user_name):
+    def get(self, request):
         return render(request, 'users/user.html', {'user_name' : 'profile'})
-    def post(self, request, user_name):
+    def post(self, request):
         return render(request, 'users/user.html', {'user_name' : 'profile'})
 
 class LoginView(View):
