cf018ed13711b54492c9506ade9df89048d6305b
Adrian Rutkowski <adrian.rutkowsky@gmail.com>
2014-04-06 02:13:20 -0700

    Add Account and Settings models with tests

--- src/apps/users/models.py
+++ src/apps/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from apps.pastes.models import Paste
 
-# Create your models here.
+class Settings(models.Model):
+    default_visibility =  models.CharField(max_length=7, choices=Paste.VISIBILITY_CHOICES)
+    default_syntax = models.CharField(max_length=20, choices=Paste.SYNTAX_CHOICES)
+
+class Account(models.Model):
+    user = models.OneToOneField(User)
+    settings = models.OneToOneField(Settings)
