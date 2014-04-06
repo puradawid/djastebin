f97e45c632b648af96ecb1d7a65248aeb031f2f4
Dawid Pura <puradawid@gmail.com>
2014-04-06 15:11:26 +0200

    #CHANGE: view and template changed to work with form class

--- src/apps/pastes/views.py
+++ src/apps/pastes/views.py
@@ -4,6 +4,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
 from apps.pastes.models import Paste
+from apps.pastes.forms import CreatePasteForm
 
 # Utility imports
 from datetime import timedelta, date
@@ -12,28 +13,33 @@ from datetime import timedelta, date
 
 class CreatePasteView(View):
     def get(self, request):
-        return render(request, 'pastes/create_paste.html')
+	form = CreatePasteForm()
+        return render(request, 'pastes/create_paste.html', {'form' : form })
     def post(self, request):
 	#retrieve data
-	title = request.POST["name"]
-	expire_date = request.POST["expiration"]
-	content = request.POST["content"]
-	visibility = request.POST["visibility"]
-	syntax = request.POST["syntax"]
-		
-	if expire_date == "0":      # if expire date is never 
-		expire_date = None  # put null into variable
-	else:                       # else calculate a date of expire
-		expire_date = date.today() + timedelta(minutes=int(expire_date))        
-
-	if not request.user.is_anonymous(): 
-		user = request.user
-	else:
-		user = None   	    # IMPORTANT: user can be null!
-	#paste creation
-	paste = Paste.objects.create(title=title, expire_date=expire_date, content=content, visibility = visibility, author=user, language=syntax)	
 	
-	return redirect(str(paste.id) + "/") 
+	
+	form = CreatePasteForm(request.POST)
+	if form.is_valid():
+		title = form.cleaned_data["title"]
+		expire_date = form.cleaned_data["expiration"]
+		content = form.cleaned_data["content"]
+		visibility = form.cleaned_data["visibility"]
+		syntax = form.cleaned_data["syntax"]	
+		if expire_date == "0":      # if expire date is never 
+			expire_date = None  # put null into variable
+		else:                       # else calculate a date of expire
+			expire_date = date.today() + timedelta(minutes=int(expire_date))        
+	
+		if not request.user.is_anonymous(): 
+			user = request.user
+		else:
+			user = None   	    # IMPORTANT: user can be null!
+		#paste creation
+		paste = Paste.objects.create(title=title, expire_date=expire_date, content=content, visibility = visibility, author=user, language=syntax)	
+		
+		return redirect(str(paste.id) + "/")
+	return render(request, 'pastes/create_paste.html', {'form' : form })
 
 class ReadPasteView(View):
     def get(self, request, paste_id):
