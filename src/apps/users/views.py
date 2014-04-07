73a3429f3b1b00c6e1981a970af8daac36246153
Paweł Kondraciuk <kondraciukpawel@gmail.com>
2014-04-07 21:32:22 +0200

    Add user pastes, Correct user authentication

--- src/apps/users/views.py
+++ src/apps/users/views.py
@@ -1,29 +1,36 @@
 from django.shortcuts import render
 from django.views.generic.base import View
-from apps.users.forms import UserRegistrationForm
-from django.contrib.auth import authenticate, login, logout
 from django.http.response import HttpResponseRedirect
-from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
 from apps.pastes.models import Paste
 from django.views.generic.list import ListView
-
 # Create your views here.
 
 class UserDetailsView(ListView):
     model = Paste
     template_name = 'users/user_pastes.html'
     context_object_name = 'paste_list'
-    paginate_by = 1
+    paginate_by = 10
     
     def get_queryset(self):
-        return Paste.objects.filter(author=User.objects.filter(username=self.kwargs['user_name']))
+        if self.request.user.is_authenticated(): 
+            return Paste.objects.filter(author=self.user_profile).order_by('-created')
+        return Paste.objects.filter(author=self.user_profile, visibility='PUBLIC').order_by('-created')
     
     def get_context_data(self, **kwargs):
         context = super(UserDetailsView, self).get_context_data(**kwargs)
-        context['user_profile'] = User.objects.get(username=self.kwargs['user_name'])
-        context['total_pastes'] = self.get_queryset().count()
+        context.update({
+            'user_profile': self.user_profile,
+            'total_pastes': self.get_queryset().count(),
+        })
         return context
+    
+    def get(self, *args, **kwargs):
+        try:
+            self.user_profile = User.objects.get(username=kwargs['user_name'])
+        except User.DoesNotExist:
+            return HttpResponseRedirect("/")
+        return super(UserDetailsView, self).get(*args, **kwargs);
 
 class SettingsView(View):
     def get(self, request):
@@ -36,36 +43,3 @@ class ProfileView(View):
         return render(request, 'users/user.html', {'user_name' : 'profile'})
     def post(self, request):
         return render(request, 'users/user.html', {'user_name' : 'profile'})
-
-class LoginView(View):
-    def get(self, request):
-        return render(request, 'users/login.html', {'form' : AuthenticationForm()})
-    def post(self, request):
-        form = AuthenticationForm(data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            if user is not None:
-                login(request, user)
-                return HttpResponseRedirect("/")
-        return render(request, 'users/login.html', {'form' : form})
-    
-class LogoutView(View):
-    def get(self, request):
-        if request.user.is_authenticated():
-            logout(request)
-        return HttpResponseRedirect("/")
-
-class RegistrationView(View):
-    def get(self, request):
-        return render(request, 'users/registration.html', {'form' : UserRegistrationForm()})
-    def post(self, request):
-        form = UserRegistrationForm(request.POST)
-        if form.is_valid():
-            username = form.clean_username()
-            password = form.clean_password2()
-            form.save()
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-            return HttpResponseRedirect("../")
-        return render(request, 'users/registration.html', {'form' : form})
--- src/djastebin/urls.py
+++ src/djastebin/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from django_common.decorators import anonymous_required
+import django.contrib.auth.views
+from django.views.generic.edit import CreateView
+from apps.users.forms import UserRegistrationForm
 admin.autodiscover()
 
 import apps.pastes.views
@@ -11,10 +15,15 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^settings/$', apps.users.views.SettingsView.as_view(), name='settings'),
     url(r'^profile/$', apps.users.views.ProfileView.as_view(), name='profile'),
-    url(r'^login/$', apps.users.views.LoginView.as_view(), name='login'),
-    url(r'^logout/$', apps.users.views.LogoutView.as_view(), name='logout'),
-    url(r'^registration/$', apps.users.views.RegistrationView.as_view(), name='registration'),
-    url(r'^trends/$', apps.pastes.views.TrendingPastesView.as_view(), name='trends'),
+    url(r'^login/$', anonymous_required(django.contrib.auth.views.login), {'template_name': 'users/login.html'}, name='login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
+    url('^registration/$', anonymous_required(CreateView.as_view(
+            template_name='register.html',
+            form_class=UserRegistrationForm,
+            success_url='/'
+    )), name='registration'),
+    url(r'^trends/$', apps.pastes.views.TrendingPastesView.as_view(), name='trends', kwargs={'days' : '1'}),
+    url(r'^trends/(?P<days>1|7|30|365|all)/$', apps.pastes.views.TrendingPastesView.as_view(), name='trends_by_day'),
     url(r'^u/', include('apps.users.urls')),
     url(r'^', include('apps.pastes.urls')),
 )
