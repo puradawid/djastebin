c67537ff1c698558e0e4c717a3c1edaab5431137
Dawid Pura <puradawid@gmail.com>
2014-04-08 17:09:52 +0200

    Changes...

--- src/apps/pastes/templatetags/syntax_highlighting.py
+++ src/apps/pastes/templatetags/syntax_highlighting.py
@@ -1,10 +1,19 @@
+# ./apps/pastes/templatetags/syntax_highlighting.pyy
+
+#Django imports
 from django import template
+
+#Pygments imports
 from pygments import highlight
 from pygments.lexers import PythonLexer
 from pygments.formatters import HtmlFormatter
 
 register = template.Library()
 
+#Filters as methods
+
 @register.filter
 def syntax_highlight(text, language):
-	return highlight(text, get_lexer_by_name(language), HtmlFormatter())
+    if language != None
+        return highlight(text, get_lexer_by_name(language), HtmlFormatter())
+    return text
