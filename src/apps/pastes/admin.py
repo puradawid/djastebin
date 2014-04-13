06baa83c68387d7e032d1f14a48f1b9c93c7c932
Adrian Rutkowski <adrian.rutkowsky@gmail.com>
2014-04-13 18:50:57 +0200

    Add paste admin panel

--- src/apps/pastes/admin.py
+++ src/apps/pastes/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from apps.pastes.models import Paste
 
-# Register your models here.
+class PasteAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return False
+    
+    fieldsets = [
+        (None,          {'fields': ['content']}),
+        ('Settings',    {'fields': ['title', 'syntax', 'visibility', 'expire_date']}),
+    ]
+    list_display = ('title', 'created', 'syntax', 'visibility', 'expire_date', 'hits', 'size')
+    list_filter = ['created', 'syntax', 'visibility']
+    search_fields = ['title']
+
+admin.site.register(Paste, PasteAdmin)
