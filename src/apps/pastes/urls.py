033bc66d6aee8982143806da02dbae206abf83f5
Adrian Rutkowski <adrian.rutkowsky@gmail.com>
2014-04-16 11:20:24 +0200

    Add comment delete by user function

--- src/apps/pastes/urls.py
+++ src/apps/pastes/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     url(r'^(?P<pk>\w+)/', include([
         url(r'^edit/$', views.UpdatePasteView.as_view(), name='update_paste'),
         url(r'^delete/$', views.DeletePasteView.as_view(), name='delete_paste'),
+        url(r'^delete-comment/$', views.DeleteCommentView.as_view(), name='delete_comment'),
         url(r'^$', views.ReadPasteView.as_view(), name='show_paste'),
     ])),
 )
